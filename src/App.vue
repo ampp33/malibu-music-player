35b4d075bb3f46e0dd5809cc64aa403ee08de02d
ampp33 <ampp33@gmail.com>
2023-11-25 16:08:42 -0600

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,33 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://www.electronjs.org/" target="_blank">
-      <img src="./assets/electron.svg" class="logo electron" alt="Electron logo" />
-    </a>
-    <a href="https://vitejs.dev/" target="_blank">
-      <img src="./assets/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Electron + Vite + Vue" />
-  <div class="flex-center">
-    Place static files into the <code>/public</code> folder
-    <img style="width: 2.4em; margin-left: .4em;" src="/logo.svg" alt="Logo">
+  <div @drop.prevent="onDrop" @dragenter.prevent @dragover.prevent style="width: 100%; height: 100%; background-color: antiquewhite; position: absolute;">
+    Hey Dude
   </div>
 </template>
 
+<script lang="ts">
+// import { contextBridge, ipcRenderer } from 'electron'
+
+export default {
+  data() {
+    return {
+      tracks: []
+    }
+  },
+  methods: {
+    onDrop(e : any) {
+      if(e.dataTransfer?.files) {
+        for(const file of e.dataTransfer.files) {
+          console.log(file)
+        }
+      }
+    }
+  },
+  mounted() {
+
+  }
+}
+</script>
+
 <style>
-.flex-center {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
 
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo.electron:hover {
-  filter: drop-shadow(0 0 2em #9FEAF9);
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
